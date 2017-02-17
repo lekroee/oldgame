0098c2bec2c3a68e821e218a0ae0e2544645d24d
seaanemily <emilybaldwin@outlook.com>
2017-02-17 11:37:51 -0700

    No

--- Old-game/BombsAway/Program.cs
+++ Old-game/BombsAway/Program.cs
@@ -17,6 +17,7 @@ namespace BombsAway
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //no
         }
     }
 }
